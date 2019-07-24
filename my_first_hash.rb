8469d548628a641c2141fbacd4d762b4f8b9bf59
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-24 19:01:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -7,7 +7,7 @@ end
 def shipping_manifest
   the_manifest = {
   "whale bone corset" => 5,
-  "porcelain vase" => 2,
+  "porcelain vases" => 2,
   "oil painting" => 3
 }
     # set a variable called `the_manifest`, equal to a hash
